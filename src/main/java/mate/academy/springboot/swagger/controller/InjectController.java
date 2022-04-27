026ff33ec4b9b208389be92dcdfc76ffc5bc58d7
AlexPravda1 <kabinkos@gmail.com>
2022-04-27 19:18:59 +0400

    - inject controller to use Service Layer

--- src/main/java/mate/academy/springboot/swagger/controller/InjectController.java
+++ src/main/java/mate/academy/springboot/swagger/controller/InjectController.java
@@ -4,7 +4,7 @@ import io.swagger.annotations.ApiOperation;
 import java.math.BigDecimal;
 import lombok.RequiredArgsConstructor;
 import mate.academy.springboot.swagger.model.Product;
-import mate.academy.springboot.swagger.repository.ProductRepository;
+import mate.academy.springboot.swagger.service.ProductService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/inject")
 @RequiredArgsConstructor
 public class InjectController {
-    private final ProductRepository productRepository;
+    private final ProductService productService;
 
     @GetMapping
     @ApiOperation(value = "Add fake Products to H2 DB for test purposes")
@@ -23,7 +23,7 @@ public class InjectController {
             Product product = new Product();
             product.setTitle("Product #" + i);
             product.setPrice(BigDecimal.valueOf(Math.random() * 1000 + 1));
-            productRepository.save(product);
+            productService.save(product);
         }
         return count + " products have been injected";
     }
