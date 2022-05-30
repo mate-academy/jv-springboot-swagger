cd902079c7fd167c7c4a4d4bd9b97895601f89f0
sltnsrh <lifenjoy8888@gmail.com>
2022-05-30 14:49:43 +0300

    created controller with crud

---
+++ src/main/java/mate/academy/springboot/swagger/controller/ProductController.java
@@ -0,0 +1,65 @@
+package mate.academy.springboot.swagger.controller;
+
+import java.util.List;
+import java.util.stream.Collectors;
+import lombok.RequiredArgsConstructor;
+import mate.academy.springboot.swagger.model.Product;
+import mate.academy.springboot.swagger.model.dto.ProductRequestDto;
+import mate.academy.springboot.swagger.model.dto.ProductResponseDto;
+import mate.academy.springboot.swagger.service.ProductMapper;
+import mate.academy.springboot.swagger.service.ProductService;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/products")
+public class ProductController {
+    private final ProductMapper mapper;
+    private final ProductService productService;
+
+    @PostMapping
+    public ProductResponseDto create(@RequestBody ProductRequestDto productRequestDto) {
+        Product product = productService.add(mapper.mapToProduct(productRequestDto));
+        return mapper.mapToDto(product);
+    }
+
+    @GetMapping("/{id}")
+    public ProductResponseDto get(@PathVariable Long id) {
+        return mapper.mapToDto(productService.get(id));
+    }
+
+    @PutMapping("{id}")
+    public ProductResponseDto update(@RequestBody ProductRequestDto productRequestDto,
+                                     @PathVariable Long id) {
+        Product product = productService.get(id);
+        product.setPrice(productRequestDto.getPrice());
+        product.setTitle(productRequestDto.getTitle());
+        return mapper.mapToDto(productService.update(product));
+    }
+
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    void delete(@PathVariable Long id) {
+        productService.delete(id);
+    }
+
+    @GetMapping
+    public List<ProductResponseDto> findAll(@RequestParam (defaultValue = "10") Integer count,
+                                            @RequestParam (defaultValue = "0") Integer page) {
+        PageRequest pageRequest = PageRequest.of(page, count);
+        return productService.findAll(pageRequest).stream()
+                .map(mapper::mapToDto)
+                .collect(Collectors.toList());
+    }
+}
---
+++ src/main/java/mate/academy/springboot/swagger/service/impl/ProductServiceImpl.java
@@ -0,0 +1,40 @@
+package mate.academy.springboot.swagger.service.impl;
+
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import mate.academy.springboot.swagger.model.Product;
+import mate.academy.springboot.swagger.repository.ProductRepository;
+import mate.academy.springboot.swagger.service.ProductService;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class ProductServiceImpl implements ProductService {
+    private final ProductRepository productRepository;
+
+    @Override
+    public Product add(Product product) {
+        return productRepository.save(product);
+    }
+
+    @Override
+    public Product get(Long id) {
+        return productRepository.getById(id);
+    }
+
+    @Override
+    public void delete(Long id) {
+        productRepository.deleteById(id);
+    }
+
+    @Override
+    public Product update(Product product) {
+        return productRepository.save(product);
+    }
+
+    @Override
+    public List<Product> findAll(PageRequest pageRequest) {
+        return productRepository.findAll(pageRequest).toList();
+    }
+}
