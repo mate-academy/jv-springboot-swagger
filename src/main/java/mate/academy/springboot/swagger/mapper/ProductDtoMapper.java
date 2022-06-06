4f48f163ad4d3e1085ec8083aeb35cd0dcae6d33
d.skarzh <d.skarzh@protonmail.com>
2022-06-06 07:57:19 +0300

    Spring Boot Swagger HW Skarzhynets

---
+++ src/main/java/mate/academy/springboot/swagger/mapper/ProductDtoMapper.java
@@ -0,0 +1,27 @@
+package mate.academy.springboot.swagger.mapper;
+
+import mate.academy.springboot.swagger.dto.ProductRequestDto;
+import mate.academy.springboot.swagger.dto.ProductResponseDto;
+import mate.academy.springboot.swagger.model.Product;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ProductDtoMapper implements ResponseDtoMapper<ProductResponseDto, Product>,
+        RequestDtoMapper<ProductRequestDto, Product> {
+    @Override
+    public Product mapToModel(ProductRequestDto dto) {
+        Product product = new Product();
+        product.setTitle(dto.getTitle());
+        product.setPrice(dto.getPrice());
+        return product;
+    }
+
+    @Override
+    public ProductResponseDto mapToDto(Product product) {
+        ProductResponseDto dto = new ProductResponseDto();
+        dto.setId(product.getId());
+        dto.setTitle(product.getTitle());
+        dto.setPrice(product.getPrice());
+        return dto;
+    }
+}
