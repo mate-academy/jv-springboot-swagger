1ed349bafcb102f10bdb660776d408b49d6e831a
Iryna Hrynda <hryndairyna1@gmail.com>
2022-06-21 18:45:19 +0300

    - implemented springboot swagger

---
+++ src/main/java/mate/academy/springboot/swagger/controller/ProductController.java
@@ -0,0 +1,104 @@
+package mate.academy.springboot.swagger.controller;
+
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
+import java.math.BigDecimal;
+import java.util.List;
+import java.util.stream.Collectors;
+import mate.academy.springboot.swagger.dto.ProductRequestDto;
+import mate.academy.springboot.swagger.dto.ProductResponseDto;
+import mate.academy.springboot.swagger.mapper.ProductMapper;
+import mate.academy.springboot.swagger.model.Product;
+import mate.academy.springboot.swagger.service.ProductService;
+import mate.academy.springboot.swagger.service.SortService;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/products")
+public class ProductController {
+    private final ProductService productService;
+    private final ProductMapper productMapper;
+    private final SortService sortService;
+
+    public ProductController(ProductService productService,
+                             ProductMapper productMapper,
+                             SortService sortService) {
+        this.productService = productService;
+        this.productMapper = productMapper;
+        this.sortService = sortService;
+    }
+
+    @PostMapping
+    @ApiOperation(value = "Create a new product")
+    public ProductResponseDto create(@RequestBody ProductRequestDto productRequestDto) {
+        Product product = productService.save(productMapper.mapToModel(productRequestDto));
+        return productMapper.mapToDto(product);
+    }
+
+    @GetMapping("/{id}")
+    @ApiOperation(value = "Get product by id")
+    public ProductResponseDto getById(@PathVariable Long id) {
+        return productMapper.mapToDto(productService.getById(id));
+    }
+
+    @DeleteMapping("/{id}")
+    @ApiOperation(value = "Delete product by id")
+    public void delete(@PathVariable Long id) {
+        productService.deleteById(id);
+    }
+
+    @PutMapping("/{id}")
+    @ApiOperation(value = "Update product by id")
+    public ProductResponseDto update(@PathVariable Long id,
+                                     @RequestBody ProductRequestDto productRequestDto) {
+        Product product = productMapper.mapToModel(productRequestDto);
+        product.setId(id);
+        return productMapper.mapToDto(productService.save(product));
+    }
+
+    @GetMapping
+    @ApiOperation(value = "Get all products")
+    public List<ProductResponseDto> getAllProducts(
+            @RequestParam(defaultValue = "20")
+            @ApiParam(value = "default value is 20") Integer count,
+            @RequestParam(defaultValue = "0")
+            @ApiParam(value = "default value is 0") Integer page,
+            @RequestParam(defaultValue = "id")
+            @ApiParam(value = "default value is id") String sortBy) {
+        Sort sort = Sort.by(sortService.parseSortOrders(sortBy));
+        PageRequest pageRequest = PageRequest.of(page, count, sort);
+        return productService.findAll(pageRequest)
+                .stream()
+                .map(productMapper::mapToDto)
+                .collect(Collectors.toList());
+    }
+
+    @GetMapping("/by-price")
+    @ApiOperation(value = "Get all product in price range")
+    public List<ProductResponseDto> getAllByPriceBetween(
+            @RequestParam BigDecimal from,
+            @RequestParam BigDecimal to,
+            @RequestParam(defaultValue = "20")
+            @ApiParam(value = "default value is 20") Integer count,
+            @RequestParam(defaultValue = "0")
+            @ApiParam(value = "default value is 0") Integer page,
+            @RequestParam(defaultValue = "id")
+            @ApiParam(value = "default value is id") String sortBy) {
+        Sort sort = Sort.by(sortService.parseSortOrders(sortBy));
+        PageRequest pageRequest = PageRequest.of(page, count, sort);
+        return productService.findAllByPriceBetween(from, to, pageRequest)
+                .stream()
+                .map(productMapper::mapToDto)
+                .collect(Collectors.toList());
+    }
+}
