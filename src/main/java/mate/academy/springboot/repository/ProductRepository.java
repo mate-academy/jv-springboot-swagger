29a77fe9f41196cd24c8f75289182f9f121cb9fd
Ksenia Bykh <ksenia.bykh@gmail.com>
2022-07-02 16:11:04 +0200

    product service was created

--- src/main/java/mate/academy/springboot/repository/ProductRepository.java
+++ src/main/java/mate/academy/springboot/repository/ProductRepository.java
@@ -4,10 +4,11 @@ import java.awt.print.Pageable;
 import java.math.BigDecimal;
 import java.util.List;
 import mate.academy.springboot.model.Product;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ProductRepository extends JpaRepository<Product, Long> {
-    List<Product> findAllByPriceBetween(BigDecimal from, BigDecimal to, Pageable pageable);
+    List<Product> findAllByPriceBetween(BigDecimal from, BigDecimal to, PageRequest pageRequest);
 }
