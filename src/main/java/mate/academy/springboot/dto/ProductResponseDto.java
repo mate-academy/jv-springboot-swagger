8b1359c8966fa66cd63de3a38af1f1cbdf628b75
Ksenia Bykh <ksenia.bykh@gmail.com>
2022-07-02 16:25:02 +0200

    dto were created

---
+++ src/main/java/mate/academy/springboot/dto/ProductResponseDto.java
@@ -0,0 +1,12 @@
+package mate.academy.springboot.dto;
+
+import java.math.BigDecimal;
+import lombok.Data;
+
+@Data
+public class ProductResponseDto {
+    private Long id;
+    private String title;
+    private BigDecimal price;
+
+}
