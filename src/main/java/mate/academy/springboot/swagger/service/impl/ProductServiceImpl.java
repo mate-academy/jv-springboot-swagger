34cec7f583f76adfca1970188b9091de7da850d7
Andrii Synelnyk <sinelnik2154@gmail.com>
2022-08-03 02:14:42 +0300

    Fixed code style

--- src/main/java/mate/academy/springboot/swagger/service/impl/ProductServiceImpl.java
+++ src/main/java/mate/academy/springboot/swagger/service/impl/ProductServiceImpl.java
@@ -37,7 +37,8 @@ public class ProductServiceImpl implements ProductService {
     }
 
     @Override
-    public List<Product> findAllByPriceBetween(BigDecimal from, BigDecimal to, PageRequest pageRequest) {
+    public List<Product> findAllByPriceBetween(BigDecimal from, BigDecimal to,
+                                               PageRequest pageRequest) {
         return productRepository.findAllByPriceBetween(from, to, pageRequest);
     }
 }
