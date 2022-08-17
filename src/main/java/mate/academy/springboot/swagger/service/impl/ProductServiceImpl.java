24da16f8448fc4b648c38bdb150fe251996872dd
Zinchuk-Yevhenii-Mate <zenya87@gmail.com>
2022-08-17 20:31:44 +0300

    first commit

---
+++ src/main/java/mate/academy/springboot/swagger/service/impl/ProductServiceImpl.java
@@ -0,0 +1,42 @@
+package mate.academy.springboot.swagger.service.impl;
+
+import java.math.BigDecimal;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import mate.academy.springboot.swagger.model.Product;
+import mate.academy.springboot.swagger.repository.ProductRepository;
+import mate.academy.springboot.swagger.service.ProductService;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
+public class ProductServiceImpl implements ProductService {
+    private final ProductRepository productRepository;
+
+    @Override
+    public Product save(Product product) {
+        return productRepository.save(product);
+    }
+
+    @Override
+    public Product getDyId(Long id) {
+        return productRepository.getById(id);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        productRepository.deleteById(id);
+    }
+
+    @Override
+    public List<Product> findAllByPriceBetween(BigDecimal fromPrice, BigDecimal toPrice,
+                                               PageRequest pageRequest) {
+        return productRepository.findAllByPriceBetween(fromPrice, toPrice, pageRequest);
+    }
+
+    @Override
+    public List<Product> findAll(PageRequest pageRequest) {
+        return productRepository.findAll(pageRequest).toList();
+    }
+}
