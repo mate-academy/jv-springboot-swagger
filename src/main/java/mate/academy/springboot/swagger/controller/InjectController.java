d57d1626e981d3162da774809469944c341b40e9
Denys.Sh <7860denis@gmail.com>
2022-08-18 18:15:41 +0300

    fixed

--- src/main/java/mate/academy/springboot/swagger/controller/InjectController.java
+++ src/main/java/mate/academy/springboot/swagger/controller/InjectController.java
@@ -24,21 +24,20 @@ public class InjectController {
     @GetMapping
     @ApiOperation(value = "Inject test data in database")
     public String injectData() {
-        productService.create(getProduct("Asus Expert Book", BigDecimal.valueOf(1755)));
-        productService.create(getProduct("Mac Pro M1", BigDecimal.valueOf(1245)));
-        productService.create(getProduct("iPhone 13Pro", BigDecimal.valueOf(1499)));
-        productService.create(getProduct("iPhone 11", BigDecimal.valueOf(755)));
-        productService.create(getProduct("iPhone 12Max", BigDecimal.valueOf(895)));
-
+        createProduct("Asus Expert Book", BigDecimal.valueOf(1755));
+        createProduct("Mac Pro M1", BigDecimal.valueOf(1245));
+        createProduct("iPhone 13Pro", BigDecimal.valueOf(1499));
+        createProduct("iPhone 11", BigDecimal.valueOf(755));
+        createProduct("iPhone 12Max", BigDecimal.valueOf(895));
         String time = ", end: " + LocalDateTime.now()
                 .format(DateTimeFormatter.ofPattern("dd-MM-YYYY HH:mm:ss"));
         return "Successful inject is done" + time;
     }
 
-    private Product getProduct(String title, BigDecimal price) {
+    private Product createProduct(String title, BigDecimal price) {
         Product product = new Product();
         product.setTitle(title);
         product.setPrice(price);
-        return product;
+        return productService.create(product);
     }
 }
