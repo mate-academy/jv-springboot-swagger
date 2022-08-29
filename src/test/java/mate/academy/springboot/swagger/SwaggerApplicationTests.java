24f40dc27bf16d40b498349c89b371339d7ba90a
Mykyta Semisoshenko <msemisoshenko@gmail.com>
2022-08-29 14:30:18 +0200

    fixed test class error

--- src/test/java/mate/academy/springboot/swagger/SwaggerApplicationTests.java
+++ src/test/java/mate/academy/springboot/swagger/SwaggerApplicationTests.java
@@ -3,7 +3,7 @@ package mate.academy.springboot.swagger;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = SwaggerApplication.class)
 class SwaggerApplicationTests {
 
     @Test
