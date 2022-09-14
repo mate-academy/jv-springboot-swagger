59606d944e6554322a371fa12b8f88ec3f0c8992
Maryna Blank <marinablank96@gmail.com>
2022-09-14 19:09:27 +0300

    swagger final

--- src/main/resources/db/changelog/changes/add-products.sql
+++ src/main/resources/db/changelog/changes/add-products.sql
@@ -1,50 +1,51 @@
-INSERT INTO products (title, price) VALUES ('Apple iPhone 14 Pro Max', 1450);
-INSERT INTO products (title, price) VALUES ('Apple iPhone SE', 370);
-INSERT INTO products (title, price) VALUES ('Apple iPhone 13 Pro Max', 860);
-INSERT INTO products (title, price) VALUES ('Acer Iconia Talk S', 170);
-INSERT INTO products (title, price) VALUES ('Acer Liquid Z6 Plus', 250);
-INSERT INTO products (title, price) VALUES ('Acer Liquid Z6', 120);
-INSERT INTO products (title, price) VALUES ('Asus Zenfone 9', 700);
-INSERT INTO products (title, price) VALUES ('Asus ROG Phone 6 Pro', 1900);
-INSERT INTO products (title, price) VALUES ('Asus ROG Phone 6', 725);
-INSERT INTO products (title, price) VALUES ('BlackBerry KEY2 LE', 440);
-INSERT INTO products (title, price) VALUES ('BlackBerry Evolve X', 350);
-INSERT INTO products (title, price) VALUES ('BlackBerry Evolve', 300);
-INSERT INTO products (title, price) VALUES ('Dell XPS 10', 380);
-INSERT INTO products (title, price) VALUES ('Dell Streak Pro D43', 450);
-INSERT INTO products (title, price) VALUES ('Dell Streak 7', 210);
-INSERT INTO products (title, price) VALUES ('Ericsson R600', 10);
-INSERT INTO products (title, price) VALUES ('Ericsson T68', 10);
-INSERT INTO products (title, price) VALUES ('Ericsson T66', 5);
-INSERT INTO products (title, price) VALUES ('Google Pixel 6a', 360);
-INSERT INTO products (title, price) VALUES ('Google Pixel 6 Pro', 550);
-INSERT INTO products (title, price) VALUES ('Google Pixel 6', 480);
-INSERT INTO products (title, price) VALUES ('Huawei Mate 50 RS Porsche Design', 1880);
-INSERT INTO products (title, price) VALUES ('Huawei Mate 50 Pro', 990);
-INSERT INTO products (title, price) VALUES ('Huawei Mate 50E', 580);
-INSERT INTO products (title, price) VALUES ('Lenovo Legion Y70', 440);
-INSERT INTO products (title, price) VALUES ('Lenovo Legion Y90', 570);
-INSERT INTO products (title, price) VALUES ('LG W41 Pro', 180);
-INSERT INTO products (title, price) VALUES ('LG W41+', 170);
-INSERT INTO products (title, price) VALUES ('LG W41', 160);
-INSERT INTO products (title, price) VALUES ('Microsoft Lumia 650', 150);
-INSERT INTO products (title, price) VALUES ('Microsoft Lumia 950 XL Dual SIM', 350);
-INSERT INTO products (title, price) VALUES ('Microsoft Lumia 950 XL', 350);
-INSERT INTO products (title, price) VALUES ('Motorola Edge 30 Ultra', 900);
-INSERT INTO products (title, price) VALUES ('Motorola Edge 30 Fusion', 600);
-INSERT INTO products (title, price) VALUES ('Nokia X30', 520);
-INSERT INTO products (title, price) VALUES ('Nokia G60', 415);
-INSERT INTO products (title, price) VALUES ('Nokia C31', 130);
-INSERT INTO products (title, price) VALUES ('Panasonic Eluga I7', 125);
-INSERT INTO products (title, price) VALUES ('Philips PH2', 110);
-INSERT INTO products (title, price) VALUES ('Samsung Galaxy A04s', 170);
-INSERT INTO products (title, price) VALUES ('Samsung Galaxy A23 5G', 300);
-INSERT INTO products (title, price) VALUES ('Sony Xperia 5 IV', 1000);
-INSERT INTO products (title, price) VALUES ('Sony Ericsson Xperia Arc S', 210);
-INSERT INTO products (title, price) VALUES ('Sony Ericsson Xperia neo V', 150);
-INSERT INTO products (title, price) VALUES ('Toshiba Windows Phone IS12T', 590);
-INSERT INTO products (title, price) VALUES ('Toshiba K01', 100);
-INSERT INTO products (title, price) VALUES ('Vodafone Smart V10', 180);
-INSERT INTO products (title, price) VALUES ('Vodafone Smart N10', 150);
-INSERT INTO products (title, price) VALUES ('Xiaomi Redmi 11 Prime', 160);
-INSERT INTO products (title, price) VALUES ('Vertu Signature Cobra', 310000);
+INSERT INTO products (title, price) VALUES
+('Apple iPhone 14 Pro Max', 1450),
+('Apple iPhone SE', 370),
+('Apple iPhone 13 Pro Max', 860),
+('Acer Iconia Talk S', 170),
+('Acer Liquid Z6 Plus', 250),
+('Acer Liquid Z6', 120),
+('Asus Zenfone 9', 700),
+('Asus ROG Phone 6 Pro', 1900),
+('Asus ROG Phone 6', 725),
+('BlackBerry KEY2 LE', 440),
+('BlackBerry Evolve X', 350),
+('BlackBerry Evolve', 300),
+('Dell XPS 10', 380),
+('Dell Streak Pro D43', 450),
+('Dell Streak 7', 210),
+('Ericsson R600', 10),
+('Ericsson T68', 10),
+('Ericsson T66', 5),
+('Google Pixel 6a', 360),
+('Google Pixel 6 Pro', 550),
+('Google Pixel 6', 480),
+('Huawei Mate 50 RS Porsche Design', 1880),
+('Huawei Mate 50 Pro', 990),
+('Huawei Mate 50E', 580),
+('Lenovo Legion Y70', 440),
+('Lenovo Legion Y90', 570),
+('LG W41 Pro', 180),
+('LG W41+', 170),
+('LG W41', 160),
+('Microsoft Lumia 650', 150),
+('Microsoft Lumia 950 XL Dual SIM', 350),
+('Microsoft Lumia 950 XL', 350),
+('Motorola Edge 30 Ultra', 900),
+('Motorola Edge 30 Fusion', 600),
+('Nokia X30', 520),
+('Nokia G60', 415),
+('Nokia C31', 130),
+('Panasonic Eluga I7', 125),
+('Philips PH2', 110),
+('Samsung Galaxy A04s', 170),
+('Samsung Galaxy A23 5G', 300),
+('Sony Xperia 5 IV', 1000),
+('Sony Ericsson Xperia Arc S', 210),
+('Sony Ericsson Xperia neo V', 150),
+('Toshiba Windows Phone IS12T', 590),
+('Toshiba K01', 100),
+('Vodafone Smart V10', 180),
+('Vodafone Smart N10', 150),
+('Xiaomi Redmi 11 Prime', 160),
+('Vertu Signature Cobra', 310000);
