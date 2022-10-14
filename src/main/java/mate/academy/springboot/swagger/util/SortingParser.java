f54f25c19f839a024e3cbc3d5d1fd15a678c1a12
Roman Pylypchuk <roman.pylypchuk@yahoo.com>
2022-10-14 19:11:54 +0300

    created SortingParser.java class & implemented method parse

---
+++ src/main/java/mate/academy/springboot/swagger/util/SortingParser.java
@@ -0,0 +1,28 @@
+package mate.academy.springboot.swagger.util;
+
+import java.util.ArrayList;
+import java.util.List;
+import org.springframework.data.domain.Sort;
+import org.springframework.stereotype.Component;
+
+@Component
+public class SortingParser {
+    public Sort parse(String sortBy) {
+        List<Sort.Order> orderList = new ArrayList<>();
+        if (sortBy.contains(":")) {
+            String[] sortingFields = sortBy.split(";");
+            for (String field : sortingFields) {
+                Sort.Order order;
+                if (field.contains(":")) {
+                    String[] fieldsAndDirections = field.split(":");
+                    order = new Sort.Order(Sort.Direction.valueOf(fieldsAndDirections[1]),
+                            fieldsAndDirections[0]);
+                } else {
+                    order = new Sort.Order(Sort.Direction.ASC, field);
+                }
+                orderList.add(order);
+            }
+        }
+        return Sort.by(orderList);
+    }
+}
