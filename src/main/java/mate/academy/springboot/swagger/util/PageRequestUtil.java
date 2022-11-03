11947b7bf03dff5aafadf44d588cb4eeafae69dd
Oleksandr Semashko <semashko123456789@gmail.com>
2022-11-03 11:40:10 +0200

    kenshoo)

--- src/main/java/mate/academy/springboot/swagger/util/PageRequestUtil.java
+++ src/main/java/mate/academy/springboot/swagger/util/PageRequestUtil.java
@@ -6,16 +6,20 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort;
 
 public class PageRequestUtil {
+    private static final String SEPARATOR_ATTRIBUTES = ":";
+    private static final String SEPARATOR_PARAMS = ";";
+    private static final int INDEX_FIELD_NAME = 0;
+    private static final int INDEX_DIRECTION_VALUE = 1;
     public static PageRequest getPageRequest(Integer page, Integer count, String sortBy) {
         List<Sort.Order> orders = new ArrayList<>();
-        if (sortBy.contains(":")) {
-            String[] sortingField = sortBy.split(";");
+        if (sortBy.contains(SEPARATOR_ATTRIBUTES)) {
+            String[] sortingField = sortBy.split(SEPARATOR_PARAMS);
             for (String field : sortingField) {
                 Sort.Order order;
-                if (field.contains(":")) {
-                    String[] fieldsAndDirection = field.split(":");
-                    order = new Sort.Order(Sort.Direction.valueOf(fieldsAndDirection[1]),
-                            fieldsAndDirection[0]);
+                if (field.contains(SEPARATOR_ATTRIBUTES)) {
+                    String[] fieldsAndDirection = field.split(SEPARATOR_ATTRIBUTES);
+                    order = new Sort.Order(Sort.Direction.valueOf(fieldsAndDirection[INDEX_DIRECTION_VALUE]),
+                            fieldsAndDirection[INDEX_FIELD_NAME]);
                 } else {
                     order = new Sort.Order(Sort.Direction.DESC, field);
                 }
