a860466f4ab06fee5a3c6f8398f881d8a1bfadb5
Valentyn Slutskyi <vaaalik45@gmail.com>
2022-11-07 02:58:16 +0200

    Was added solution

---
+++ src/main/java/mate/academy/springboot/swagger/dto/ProductMapper.java
@@ -0,0 +1,22 @@
+package mate.academy.springboot.swagger.dto;
+
+import mate.academy.springboot.swagger.model.Product;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ProductMapper {
+    public ProductResponseDto toResponseDto(Product product) {
+        ProductResponseDto responseDto = new ProductResponseDto();
+        responseDto.setId(product.getId());
+        responseDto.setTitle(product.getTitle());
+        responseDto.setPrice(product.getPrice());
+        return responseDto;
+    }
+
+    public Product toProductModel(ProductRequestDto requestDto) {
+        Product product = new Product();
+        product.setTitle(requestDto.getTitle());
+        product.setPrice(requestDto.getPrice());
+        return product;
+    }
+}
---
+++ src/main/java/mate/academy/springboot/swagger/util/SortEntity.java
@@ -0,0 +1,29 @@
+package mate.academy.springboot.swagger.util;
+
+import java.util.ArrayList;
+import java.util.List;
+import org.springframework.data.domain.Sort;
+
+public class SortEntity {
+    public static List<Sort.Order> sort(String sortBy) {
+        List<Sort.Order> orders = new ArrayList<>();
+        if (sortBy.contains(":")) {
+            String[] sortingFields = sortBy.split(";");
+            for (String field : sortingFields) {
+                Sort.Order order;
+                if (field.contains(":")) {
+                    String[] fieldsAndDirections = field.split(":");
+                    order = new Sort.Order(Sort.Direction.valueOf(fieldsAndDirections[1]),
+                            fieldsAndDirections[1]);
+                } else {
+                    order = new Sort.Order(Sort.Direction.DESC, field);
+                }
+                orders.add(order);
+            }
+        } else {
+            Sort.Order order = new Sort.Order(Sort.Direction.DESC, sortBy);
+            orders.add(order);
+        }
+        return orders;
+    }
+}
