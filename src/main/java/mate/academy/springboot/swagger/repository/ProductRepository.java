37d3f2132a1cd70d09cfcd31944a5a0712d3063e
Anna Popova <anitapopova1996@gmail.com>
2022-11-10 17:38:05 +0200

    Removed custom query

--- src/main/java/mate/academy/springboot/swagger/repository/ProductRepository.java
+++ src/main/java/mate/academy/springboot/swagger/repository/ProductRepository.java
@@ -3,12 +3,11 @@ package mate.academy.springboot.swagger.repository;
 import java.math.BigDecimal;
 import java.util.List;
 import mate.academy.springboot.swagger.model.Product;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ProductRepository extends JpaRepository<Product, Long> {
-    @Query(value = "from Product  p where p.price between ?1 and ?2")
-    List<Product> getAllByPriceBetween(BigDecimal from, BigDecimal to);
+    List<Product> getAllByPriceBetween(BigDecimal from, BigDecimal to, PageRequest pageRequest);
 }
