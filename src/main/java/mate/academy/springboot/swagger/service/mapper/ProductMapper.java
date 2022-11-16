241eb842baa1fc007b51580527c4f7bfef783170
Myroslav_Voskoboinyk <maestro.of.pain@gmail.com>
2022-11-16 09:51:25 +0200

    minor style mistakes were corrected

--- src/main/java/mate/academy/springboot/swagger/service/mapper/ProductMapper.java
+++ src/main/java/mate/academy/springboot/swagger/service/mapper/ProductMapper.java
@@ -10,19 +10,19 @@ public class ProductMapper implements ResponseDtoMapper<ProductResponseDto, Prod
         RequestDtoMapper<ProductRequestDto, Product> {
 
     @Override
-    public Product mapToModel(ProductRequestDto dto) {
+    public Product mapToModel(ProductRequestDto productRequestDto) {
         Product product = new Product();
-        product.setTitle(dto.getTitle());
-        product.setPrice(dto.getPrice());
+        product.setTitle(productRequestDto.getTitle());
+        product.setPrice(productRequestDto.getPrice());
         return product;
     }
 
     @Override
     public ProductResponseDto mapToDto(Product product) {
-        ProductResponseDto dto = new ProductResponseDto();
-        dto.setId(product.getId());
-        dto.setTitle(product.getTitle());
-        dto.setPrice(product.getPrice());
-        return dto;
+        ProductResponseDto productResponseDto = new ProductResponseDto();
+        productResponseDto.setId(product.getId());
+        productResponseDto.setTitle(product.getTitle());
+        productResponseDto.setPrice(product.getPrice());
+        return productResponseDto;
     }
 }
