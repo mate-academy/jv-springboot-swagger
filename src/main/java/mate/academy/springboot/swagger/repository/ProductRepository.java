41a1e6634fd48c8d7f7245b93b87967aff9f908b
Blokitin Oleh <87024861+oleh-satoshi@users.noreply.github.com>
2022-11-18 12:31:15 +0100

    implement ProductRepository

---
+++ src/main/java/mate/academy/springboot/swagger/repository/ProductRepository.java
@@ -0,0 +1,12 @@
+package mate.academy.springboot.swagger.repository;
+
+import java.util.List;
+import java.math.BigDecimal;
+import mate.academy.springboot.swagger.model.Product;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProductRepository extends JpaRepository<Product, Long> {
+    List<Product> findAllByPriceBetween(PageRequest pageRequest, BigDecimal from, BigDecimal to);
+
+}
