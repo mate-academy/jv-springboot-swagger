c3611ed6ee7f01cbd22b3fee011e0ee87243fbda
Oleg Pruh <olegpruh1@gmail.com>
2022-12-03 09:59:00 +0000

    Task completed

---
+++ src/main/java/mate/academy/springboot/swagger/controller/ProductController.java
@@ -0,0 +1,78 @@
+package mate.academy.springboot.swagger.controller;
+
+import io.swagger.annotations.ApiOperation;
+import java.math.BigDecimal;
+import java.util.List;
+import java.util.stream.Collectors;
+import mate.academy.springboot.swagger.dto.ProductRequestDto;
+import mate.academy.springboot.swagger.dto.ProductResponseDto;
+import mate.academy.springboot.swagger.dto.mapper.ProductDtoMapper;
+import mate.academy.springboot.swagger.model.Product;
+import mate.academy.springboot.swagger.service.ProductService;
+import mate.academy.springboot.swagger.util.SortUtil;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class ProductController {
+    private ProductDtoMapper mapper;
+    private ProductService service;
+    private SortUtil sortUtil;
+
+    @PostMapping
+    @ApiOperation(value = "Save a new product")
+    public ProductResponseDto save(@RequestBody ProductRequestDto requestDto) {
+        return mapper.modelToDto(service.save(mapper.dtoToModel(requestDto)));
+    }
+
+    @GetMapping("/{id}")
+    @ApiOperation(value = "Get a product by id")
+    public ProductResponseDto getById(@PathVariable Long id) {
+        return mapper.modelToDto(service.getById(id));
+    }
+
+    @DeleteMapping
+    @ApiOperation(value = "Delete a product by id")
+    public void deleteById(@RequestParam Long id) {
+        service.deleteById(id);
+    }
+
+    @PutMapping
+    @ApiOperation(value = "Update a product")
+    public void update(@RequestBody Product product) {
+        service.update(product);
+    }
+
+    @GetMapping("/getAll")
+    @ApiOperation(value = "Get all products (page size, page number, sortBy parameters)")
+    public List<ProductResponseDto> getAll(@RequestParam (defaultValue = "3") Integer count,
+                                           @RequestParam (defaultValue = "0") Integer page,
+                                           @RequestParam (defaultValue = "id") String sortBy) {
+        Sort sort = sortUtil.parse(sortBy);
+        PageRequest pageRequest = PageRequest.of(page, count, sort);
+        return service.getAll(pageRequest).stream()
+                .map(mapper::modelToDto)
+                .collect(Collectors.toList());
+    }
+
+    @GetMapping("/getAllByPriceBetween")
+    public List<ProductResponseDto> getAllByPriceBetween(
+                @RequestParam (defaultValue = "3") Integer count,
+                @RequestParam (defaultValue = "0") Integer page,
+                @RequestParam (defaultValue = "id") String sortBy,
+                @RequestParam BigDecimal from,
+                @RequestParam BigDecimal to) {
+        PageRequest pageRequest = PageRequest.of(page, count, sortUtil.parse(sortBy));
+        return service.findAllByPriceBetween(from, to, pageRequest).stream()
+                .map(mapper::modelToDto)
+                .collect(Collectors.toList());
+    }
+}
---
+++ src/main/java/mate/academy/springboot/swagger/dto/mapper/ProductDtoMapper.java
@@ -0,0 +1,24 @@
+package mate.academy.springboot.swagger.dto.mapper;
+
+import mate.academy.springboot.swagger.dto.ProductRequestDto;
+import mate.academy.springboot.swagger.dto.ProductResponseDto;
+import mate.academy.springboot.swagger.model.Product;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class ProductDtoMapper {
+    public ProductResponseDto modelToDto(Product product) {
+        ProductResponseDto responseDto = new ProductResponseDto();
+        responseDto.setTitle(product.getTitle());
+        responseDto.setId(product.getId());
+        responseDto.setPrice(product.getPrice());
+        return responseDto;
+    }
+
+    public Product dtoToModel(ProductRequestDto requestDto) {
+        Product product = new Product();
+        product.setTitle(requestDto.getTitle());
+        product.setPrice(requestDto.getPrice());
+        return product;
+    }
+}
