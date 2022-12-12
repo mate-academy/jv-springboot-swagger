5297779befe6ef0a66dfccc6f4772eb6e264c901
Serhii Buria <sergeiburya@gmail.com>
2022-12-12 15:41:02 +0200

    added ProductService

---
+++ src/main/resources/db/changelog/changes/add-products.sql
@@ -0,0 +1,10 @@
+INSERT INTO products (title, price) VALUES ('Samsung X5 silver', 1060);
+INSERT INTO products (title, price) VALUES ('Samsung X5 gold', 1876);
+INSERT INTO products (title, price) VALUES ('Samsung X5', 1234);
+INSERT INTO products (title, price) VALUES ('Samsung X10', 567);
+INSERT INTO products (title, price) VALUES ('Samsung X22', 960);
+INSERT INTO products (title, price) VALUES ('Samsung A55', 1260);
+INSERT INTO products (title, price) VALUES ('Samsung X5', 1110);
+INSERT INTO products (title, price) VALUES ('Samsung X9', 1200);
+INSERT INTO products (title, price) VALUES ('Samsung XG45', 980);
+
