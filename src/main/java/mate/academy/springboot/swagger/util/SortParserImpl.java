ce6ad77f20de0cdabee9dcbf3c2af64f59e53ae1
apc10bbe <poka4ailo.oleg@gmail.com>
2023-01-04 23:53:35 +0200

    released REST app by Spring Boot used swagger

---
+++ src/main/java/mate/academy/springboot/swagger/util/SortParserImpl.java
@@ -0,0 +1,35 @@
+package mate.academy.springboot.swagger.util;
+
+import java.util.ArrayList;
+import java.util.List;
+import org.springframework.data.domain.Sort;
+import org.springframework.stereotype.Component;
+
+@Component
+public class SortParserImpl implements SortParser {
+    private static final int DIRECTION = 1;
+    private static final int FIELD = 0;
+
+    @Override
+    public Sort sortBy(String sortBy) {
+        List<Sort.Order> orders = new ArrayList<>();
+        if (sortBy.contains(":")) {
+            String[] sortingFields = sortBy.split(";");
+            for (String field : sortingFields) {
+                Sort.Order order;
+                if (field.contains(":")) {
+                    String[] fieldAndDirections = field.split(":");
+                    order = new Sort.Order(Sort.Direction.valueOf(fieldAndDirections[DIRECTION]),
+                            fieldAndDirections[FIELD]);
+                } else {
+                    order = new Sort.Order(Sort.Direction.DESC, field);
+                }
+                orders.add(order);
+            }
+        } else {
+            Sort.Order order = new Sort.Order(Sort.Direction.DESC, sortBy);
+            orders.add(order);
+        }
+        return Sort.by(orders);
+    }
+}
