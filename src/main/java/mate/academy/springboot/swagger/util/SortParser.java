582ed28d7c002354fac3fbb883e96f092646f80f
apc10bbe <poka4ailo.oleg@gmail.com>
2023-01-05 11:38:17 +0200

    deleted interface for util class

--- src/main/java/mate/academy/springboot/swagger/util/SortParser.java
+++ src/main/java/mate/academy/springboot/swagger/util/SortParser.java
@@ -1,7 +1,36 @@
 package mate.academy.springboot.swagger.util;
 
+import java.util.ArrayList;
+import java.util.List;
 import org.springframework.data.domain.Sort;
+import org.springframework.stereotype.Component;
 
-public interface SortParser {
-    Sort sortBy(String sortBy);
+@Component
+public class SortParser {
+    private static final int DIRECTION = 1;
+    private static final int FIELD = 0;
+    private static final String FIELD_REGEX = ";";
+    private static final String FIELD_AND_DIRECTION_REGEX = ":";
+
+    public Sort sortBy(String sortBy) {
+        List<Sort.Order> orders = new ArrayList<>();
+        if (sortBy.contains(FIELD_AND_DIRECTION_REGEX)) {
+            String[] sortingFields = sortBy.split(FIELD_REGEX);
+            for (String field : sortingFields) {
+                Sort.Order order;
+                if (field.contains(FIELD_AND_DIRECTION_REGEX)) {
+                    String[] fieldAndDirections = field.split(FIELD_AND_DIRECTION_REGEX);
+                    order = new Sort.Order(Sort.Direction.valueOf(fieldAndDirections[DIRECTION]),
+                            fieldAndDirections[FIELD]);
+                } else {
+                    order = new Sort.Order(Sort.Direction.DESC, field);
+                }
+                orders.add(order);
+            }
+        } else {
+            Sort.Order order = new Sort.Order(Sort.Direction.DESC, sortBy);
+            orders.add(order);
+        }
+        return Sort.by(orders);
+    }
 }
