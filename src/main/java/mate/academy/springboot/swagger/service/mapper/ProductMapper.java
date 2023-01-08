ed0bc97128ffb4da280a2a1cd04d001977e0abfd
Volodymyr Oliinyk <volodymyr.oliinyk89@gmail.com>
2023-01-08 10:25:15 +0200

    implemented task

---
+++ src/main/java/mate/academy/springboot/swagger/service/mapper/ProductMapper.java
@@ -0,0 +1,24 @@
+package mate.academy.springboot.swagger.service.mapper;
+
+import mate.academy.springboot.swagger.dto.request.ProductRequestDto;
+import mate.academy.springboot.swagger.dto.response.ProductResponseDto;
+import mate.academy.springboot.swagger.model.Product;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ProductMapper {
+    public ProductResponseDto toDto(Product product) {
+        ProductResponseDto productResponseDto = new ProductResponseDto();
+        productResponseDto.setId(product.getId());
+        productResponseDto.setPrice(product.getPrice());
+        productResponseDto.setTitle(product.getTitle());
+        return productResponseDto;
+    }
+
+    public Product toModel(ProductRequestDto productRequestDto) {
+        Product product = new Product();
+        product.setTitle(productRequestDto.getTitle());
+        product.setPrice(productRequestDto.getPrice());
+        return product;
+    }
+}
