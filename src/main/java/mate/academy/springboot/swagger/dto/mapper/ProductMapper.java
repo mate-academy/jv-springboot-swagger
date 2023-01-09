2b880ab606097b17232c785c934d9b5427253220
Mark Rybnikov <mark.rybnikov@amdaris.com>
2023-01-09 14:36:41 +0200

    Created product controller

---
+++ src/main/java/mate/academy/springboot/swagger/dto/mapper/ProductMapper.java
@@ -0,0 +1,25 @@
+package mate.academy.springboot.swagger.dto.mapper;
+
+import mate.academy.springboot.swagger.dto.ProductRequestDto;
+import mate.academy.springboot.swagger.dto.ProductResponseDto;
+import mate.academy.springboot.swagger.model.Product;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ProductMapper {
+
+    public ProductResponseDto toResponseDto(Product product) {
+        ProductResponseDto productResponseDto = new ProductResponseDto();
+        productResponseDto.setId(product.getId());
+        productResponseDto.setPrice(product.getPrice());
+        productResponseDto.setTitle(product.getTitle());
+        return productResponseDto;
+    }
+
+    public Product toModel(ProductRequestDto productResponseDto) {
+        Product product = new Product();
+        product.setPrice(productResponseDto.getPrice());
+        product.setTitle(productResponseDto.getTitle());
+        return product;
+    }
+}
