3d93cf7e458de4ddeef7a842a787c62b5fe1aecf
Serhii Prykhodko <sergii.prykhodko@gmail.com>
2023-01-11 00:00:25 +0200

    created mapper and dto

---
+++ src/main/java/mate/academy/springboot/swagger/dto/mapper/impl/ProductMapper.java
@@ -0,0 +1,28 @@
+package mate.academy.springboot.swagger.dto.mapper.impl;
+
+import mate.academy.springboot.swagger.dto.ProductRequestDto;
+import mate.academy.springboot.swagger.dto.ProductResponseDto;
+import mate.academy.springboot.swagger.dto.mapper.RequestDtoMapper;
+import mate.academy.springboot.swagger.dto.mapper.ResponseDtoMapper;
+import mate.academy.springboot.swagger.model.Product;
+
+public class ProductMapper implements RequestDtoMapper<ProductRequestDto, Product>,
+        ResponseDtoMapper<ProductResponseDto, Product> {
+
+    @Override
+    public Product toModel(ProductRequestDto dto) {
+        Product product = new Product();
+        product.setTitle(dto.getTitle());
+        product.setPrice(dto.getPrice());
+        return product;
+    }
+
+    @Override
+    public ProductResponseDto toDto(Product product) {
+        ProductResponseDto dto = new ProductResponseDto();
+        dto.setId(product.getId());
+        dto.setTitle(product.getTitle());
+        dto.setPrice(product.getPrice());
+        return dto;
+    }
+}
