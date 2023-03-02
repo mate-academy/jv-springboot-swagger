e1c57a7314e4ef6f569d83ac5d5af65f2e0f2401
Danyl Kniaziev <kdanil0401@gmail.com>
2023-03-02 10:29:31 +0200

    done all task

---
+++ src/main/java/mate/academy/springboot/swagger/service/impl/ProductMapperImpl.java
@@ -0,0 +1,25 @@
+package mate.academy.springboot.swagger.service.impl;
+
+import mate.academy.springboot.swagger.model.Product;
+import mate.academy.springboot.swagger.model.dto.ProductRequestDto;
+import mate.academy.springboot.swagger.model.dto.ProductResponseDto;
+import mate.academy.springboot.swagger.service.ProductMapper;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ProductMapperImpl implements ProductMapper {
+    public Product mapToModel(ProductRequestDto dto) {
+        Product product = new Product();
+        product.setTitle(dto.getTitle());
+        product.setPrice(dto.getPrice());
+        return product;
+    }
+
+    public ProductResponseDto mapToDto(Product product) {
+        ProductResponseDto dto = new ProductResponseDto();
+        dto.setId(product.getId());
+        dto.setPrice(product.getPrice());
+        dto.setTitle(dto.getTitle());
+        return dto;
+    }
+}
