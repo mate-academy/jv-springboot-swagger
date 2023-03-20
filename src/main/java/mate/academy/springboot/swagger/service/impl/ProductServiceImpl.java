1701a2ed476fcc43ef59bd39b3aab83849f5a3b5
Oleksandr Kostiukevych <kaasnake2@gmail.com>
2023-03-20 18:39:16 +0200

    springboot swagger 3d commit bugs fixed

--- src/main/java/mate/academy/springboot/swagger/service/impl/ProductServiceImpl.java
+++ src/main/java/mate/academy/springboot/swagger/service/impl/ProductServiceImpl.java
@@ -2,19 +2,17 @@ package mate.academy.springboot.swagger.service.impl;
 
 import java.math.BigDecimal;
 import java.util.List;
+import lombok.AllArgsConstructor;
 import mate.academy.springboot.swagger.model.Product;
 import mate.academy.springboot.swagger.repository.ProductRepository;
 import mate.academy.springboot.swagger.service.ProductService;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 
+@AllArgsConstructor
 @Service
 public class ProductServiceImpl implements ProductService {
     private final ProductRepository productRepository;
-
-    public ProductServiceImpl(ProductRepository productRepository) {
-        this.productRepository = productRepository;
-    }
 
     @Override
     public Product add(Product product) {
--- src/main/java/mate/academy/springboot/swagger/service/impl/SortParamParserImpl.java
+++ src/main/java/mate/academy/springboot/swagger/service/impl/SortParamParserImpl.java
@@ -8,15 +8,16 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class SortParamParserImpl implements SortParamParser {
-
+    private static final String FIELD_SEPARATOR = ";";
+    private static final String FIELD_AND_DIRECTION_SEPARATOR = ":";
     @Override
     public Sort parse(String sortParam) {
         List<Sort.Order> orders = new ArrayList<>();
-        String[] sortingFields = sortParam.split(";");
+        String[] sortingFields = sortParam.split("FIELD_SEPARATOR");
         for (String field : sortingFields) {
             Sort.Order order;
-            if (field.contains(":")) {
-                String[] fieldAndDirection = field.split(":");
+            if (field.contains("FIELD_AND_DIRECTION_SEPARATOR")) {
+                String[] fieldAndDirection = field.split("FIELD_AND_DIRECTION_SEPARATOR");
                 order = new Sort.Order(Sort.Direction.valueOf(fieldAndDirection[1]),
                         fieldAndDirection[0]);
             } else {
