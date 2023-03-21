e8464ca51c2e3eb6fe87dad26c951c3744112e4b
Dmytro Krasnopir <logoltip@gmail.com>
2023-03-21 10:52:09 +0200

    minor update

--- src/main/java/mate/academy/springboot/swagger/util/SortOrderUtil.java
+++ src/main/java/mate/academy/springboot/swagger/util/SortOrderUtil.java
@@ -9,14 +9,16 @@ import org.springframework.stereotype.Component;
 public class SortOrderUtil {
     private static final int ORDER_DIRECTIONAL_INDEX = 1;
     private static final int FIELD_INDEX = 0;
+    private static final String FIELD_SEPARATOR = ";";
+    private static final String DIRECTIONAL_AND_FIELD_SEPARATOR = ":";
     private static Sort.Order order;
 
     public Sort sort(String sortBy) {
         List<Sort.Order> orders = new ArrayList<>();
-        String[] sortingFields = sortBy.split(";");
+        String[] sortingFields = sortBy.split(FIELD_SEPARATOR);
         for (String field : sortingFields) {
-            if (field.contains(":")) {
-                String[] fieldsAndDirections = field.split(":");
+            if (field.contains(DIRECTIONAL_AND_FIELD_SEPARATOR)) {
+                String[] fieldsAndDirections = field.split(DIRECTIONAL_AND_FIELD_SEPARATOR);
                 order = new Sort.Order(
                         Sort.Direction.valueOf(fieldsAndDirections[ORDER_DIRECTIONAL_INDEX]),
                         fieldsAndDirections[FIELD_INDEX]);
@@ -24,6 +26,7 @@ public class SortOrderUtil {
                 order = new Sort.Order(Sort.Direction.DESC, field);
             }
             orders.add(order);
-        }return Sort.by(orders);
+        }
+        return Sort.by(orders);
     }
 }
