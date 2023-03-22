adbba9c1a3373838575e228fdaff1afafa98b312
Taras Zhyravetskyi <tarik.zhyravetskyy@gmail.com>
2023-03-22 09:15:17 +0200

    fixed repository

--- src/main/java/mate/academy/springboot/swagger/repository/ProductRepository.java
+++ src/main/java/mate/academy/springboot/swagger/repository/ProductRepository.java
@@ -4,10 +4,11 @@ import java.math.BigDecimal;
 import java.util.List;
 import mate.academy.springboot.swagger.entity.Product;
 import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ProductRepository extends JpaRepository<Product, Long> {
-    List<Product> findAllByPriceBetween(BigDecimal from, BigDecimal to, PageRequest pageRequest);
+    List<Product> findAllByPriceBetween(BigDecimal from, BigDecimal to, Pageable pageable);
 }
