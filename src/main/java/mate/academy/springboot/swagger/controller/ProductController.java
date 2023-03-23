b9ff0c670e989189261b5cdc774e3d6fa37d31bb
ivan-vovnyanko <moooonkit@gmail.com>
2023-03-23 21:37:49 +0200

    solution

--- src/main/java/mate/academy/springboot/swagger/controller/ProductController.java
+++ src/main/java/mate/academy/springboot/swagger/controller/ProductController.java
@@ -1,5 +1,7 @@
 package mate.academy.springboot.swagger.controller;
 
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
@@ -23,6 +25,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/products")
 public class ProductController {
+    public static final String BEFORE_KEY = "priceBefore";
+    public static final String FROM_KEY = "priceFrom";
     private final ProductService productService;
     private final RequestDtoMapper<Product, ProductRequestDto> productRequestMapper;
     private final ResponseDtoMapper<Product, ProductResponseDto> productResponseMapper;
@@ -36,22 +40,26 @@ public class ProductController {
         this.productResponseMapper = productResponseMapper;
     }
 
+    @ApiOperation(value = "Create a new product")
     @PostMapping
     public ProductResponseDto create(@RequestBody ProductRequestDto productRequestDto) {
         Product product = productRequestMapper.toModel(productRequestDto);
         return productResponseMapper.toDto(productService.create(product));
     }
 
+    @ApiOperation(value = "Get product by id")
     @GetMapping("/{id}")
     public ProductResponseDto get(@PathVariable Long id) {
         return productResponseMapper.toDto(productService.get(id));
     }
 
+    @ApiOperation(value = "Delete product by id")
     @DeleteMapping("/{id}")
     public void delete(@PathVariable Long id) {
         productService.delete(id);
     }
 
+    @ApiOperation(value = "Update product")
     @PutMapping("/{id}")
     public ProductResponseDto update(@PathVariable Long id,
                                      @RequestBody ProductRequestDto productRequestDto) {
@@ -60,19 +68,32 @@ public class ProductController {
         return productResponseMapper.toDto(productService.update(product));
     }
 
+    @ApiOperation(value = "Get product by price filter and with pagination & sort")
     @GetMapping("/filter")
-    public List<ProductResponseDto> getAllWithFilter(@RequestParam Map<String, String> param,
-                                           @RequestParam (defaultValue = "0") Integer page,
-                                           @RequestParam (defaultValue = "20") Integer size) {
-        return productService.getAll(param, page, size).stream()
+    public List<ProductResponseDto> getAllWithFilter(
+            @RequestParam (defaultValue = "999999") String priceBefore,
+            @ApiParam(value = "default value is 999999") String sortBy,
+            @RequestParam (defaultValue = "0")
+            @ApiParam(value = "default value is 0") String priceFrom,
+            @RequestParam (defaultValue = "0")
+            @ApiParam(value = "default value is 0") Integer page,
+            @RequestParam (defaultValue = "20")
+            @ApiParam(value = "default value is 20") Integer size) {
+        return productService
+                .getAll(Map.of(BEFORE_KEY, priceBefore, FROM_KEY, priceFrom), page, size).stream()
                 .map(productResponseMapper::toDto)
                 .collect(Collectors.toList());
     }
 
+    @ApiOperation(value = "Get product with pagination & sort")
     @GetMapping
-    public List<ProductResponseDto> getAll(@RequestParam String sortBy,
-                                           @RequestParam (defaultValue = "0") Integer page,
-                                           @RequestParam (defaultValue = "20") Integer size) {
+    public List<ProductResponseDto> getAll(
+            @RequestParam (defaultValue = "id")
+            @ApiParam(value = "default value is id") String sortBy,
+            @RequestParam (defaultValue = "20")
+            @ApiParam(value = "default value is 20") Integer page,
+            @RequestParam (defaultValue = "20")
+            @ApiParam(value = "default value is 20") Integer size) {
         return productService.getAll(page, size, sortBy).stream()
                 .map(productResponseMapper::toDto)
                 .collect(Collectors.toList());
