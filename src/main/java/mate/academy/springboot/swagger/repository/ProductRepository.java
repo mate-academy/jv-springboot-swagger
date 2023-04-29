fb42cdb2280c306ad861614b96b9f7e27d803d0d
Maksym Ivanov <ivanov1maxim@gmail.com>
2023-04-29 17:12:29 +0300

    Add ProductRepository

---
+++ src/main/java/mate/academy/springboot/swagger/repository/ProductRepository.java
@@ -0,0 +1,8 @@
+package mate.academy.springboot.swagger.repository;
+
+import mate.academy.springboot.swagger.model.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProductRepository extends JpaRepository<Product, Long> {
+
+}
