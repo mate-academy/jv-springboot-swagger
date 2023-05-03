1a6845fc5eaec473b9c151976b72830808808af5
Maksym Ivanov <ivanov1maxim@gmail.com>
2023-05-03 17:26:13 +0300

    Add Swagger documentation

--- src/main/java/mate/academy/springboot/swagger/controller/ProductController.java
+++ src/main/java/mate/academy/springboot/swagger/controller/ProductController.java
@@ -3,6 +3,10 @@ package mate.academy.springboot.swagger.controller;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import mate.academy.springboot.swagger.dto.ProductRequestDto;
 import mate.academy.springboot.swagger.dto.ProductResponseDto;
 import mate.academy.springboot.swagger.mapper.ProductMapper;
@@ -51,6 +55,23 @@ public class ProductController {
     }
 
     @GetMapping
+    @Operation( summary = "Get All Products | With Parameters")
+    @Parameter(name = "page",
+            allowEmptyValue = true,
+            description = "Specify the page | Should be > 0")
+    @Parameter(name = "count",
+            allowEmptyValue = true,
+            description = "Specify the amount of products | Should be > 0")
+    @Parameter(name = "sortBy",
+            allowEmptyValue = true,
+            description = "Specify the field for sorting - id, title, price "
+                    + "and sorting direction | Example title: ASC or price:DESC")
+    @Parameter(name = "priceFrom",
+            allowEmptyValue = true,
+            description = "Specify price from")
+    @Parameter(name = "priceTo",
+            allowEmptyValue = true,
+            description = "Specify price to")
     private List<ProductResponseDto> getAll(@RequestParam Map<String, String> params) {
         return service.getAll(params)
                 .stream()
--- src/main/java/mate/academy/springboot/swagger/model/Product.java
+++ src/main/java/mate/academy/springboot/swagger/model/Product.java
@@ -1,10 +1,11 @@
 package mate.academy.springboot.swagger.model;
 
 import java.math.BigDecimal;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
