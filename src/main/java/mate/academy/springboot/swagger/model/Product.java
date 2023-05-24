c1586c7fde2a0fadf6bbb612d8b4aa131d540443
Nimakel <maxim.yashyn@gmail.com>
2023-05-24 19:43:50 +0300

    created product and its request, response dto's

---
+++ src/main/java/mate/academy/springboot/swagger/model/Product.java
@@ -0,0 +1,33 @@
+package mate.academy.springboot.swagger.model;
+
+import java.math.BigDecimal;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@EqualsAndHashCode
+@Table(name = "products")
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String title;
+    private BigDecimal price;
+
+    @Override
+    public String toString() {
+        return "Product{"
+                + "id=" + id
+                + ", title='" + title + '\''
+                + ", price=" + price
+                + '}';
+    }
+}
