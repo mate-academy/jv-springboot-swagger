02148e7a112eff961e29d67c257a0325ecce9c10
Potseluiko Vitalii <vetaliy063@gmail.com>
2023-05-25 16:34:10 +0300

    Implemented pagination and swagger doc

--- src/main/java/mate/academy/springboot/swagger/controller/ProductController.java
+++ src/main/java/mate/academy/springboot/swagger/controller/ProductController.java
@@ -1,10 +1,10 @@
 package mate.academy.springboot.swagger.controller;
 
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
 import java.math.BigDecimal;
 import java.util.List;
 import java.util.stream.Collectors;
-import io.swagger.annotations.ApiOperation;
-import io.swagger.annotations.ApiParam;
 import lombok.AllArgsConstructor;
 import mate.academy.springboot.swagger.dto.ProductRequestDto;
 import mate.academy.springboot.swagger.dto.ProductResponseDto;
