9aba3a69e16b89e0153e5193188cffe187ea0b94
Yuriy Turchin <turchin.yuriy1@gmail.com>
2023-05-25 16:27:57 +0300

    pagination, sorting, swagger

---
+++ src/main/java/mate/academy/springboot/swagger/controller/ProductController.java
@@ -0,0 +1,79 @@
+package mate.academy.springboot.swagger.controller;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import lombok.RequiredArgsConstructor;
+import mate.academy.springboot.swagger.dto.ProductRequestDto;
+import mate.academy.springboot.swagger.dto.ProductResponseDto;
+import mate.academy.springboot.swagger.dto.mapper.ProductMapper;
+import mate.academy.springboot.swagger.model.Product;
+import mate.academy.springboot.swagger.service.ProductService;
+import mate.academy.springboot.swagger.util.SortUtil;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/products")
+public class ProductController {
+    private final ProductService productService;
+    private final ProductMapper productMapper;
+    private final SortUtil sortUtil;
+
+    @GetMapping
+    @Operation(summary = "Find all products")
+    public List<ProductResponseDto> findAll(@RequestParam (defaultValue = "10")
+                                                @Parameter(description = "Numbers of product on 1 page")
+                                                        Integer count,
+                                            @RequestParam (defaultValue = "0")
+                                                    @Parameter(description = "Number of page")
+                                                    Integer page,
+                                            @RequestParam (defaultValue = "id")
+                                                @Parameter(description = "Parameter for sorting")
+                                                        String sortBy) {
+        Sort sort = Sort.by(sortUtil.getSort(sortBy));
+        PageRequest pageRequest = PageRequest.of(page, count, sort);
+        return productService.findAll(pageRequest).stream()
+                .map(productMapper::toDto)
+                .collect(Collectors.toList());
+    }
+
+    @GetMapping("/{id}")
+    @Operation(summary = "Get product by id")
+    public ProductResponseDto getById(@PathVariable Long id) {
+        return productMapper.toDto(productService.findById(id));
+    }
+
+    @DeleteMapping("/{id}")
+    @Operation(summary = "Delete product by id")
+    public void delete(@PathVariable Long id) {
+        productService.delete(id);
+    }
+
+    @PutMapping("/{id}")
+    @Operation(summary = "Update product by id")
+    public ProductResponseDto update(@PathVariable Long id,
+                                     @RequestBody ProductRequestDto requestDto) {
+        Product product = productMapper.toModel(requestDto);
+        product.setId(id);
+        return productMapper.toDto(productService.update(product));
+    }
+
+    @PostMapping
+    @Operation(summary = "Create product and add to DB")
+    public ProductResponseDto save(@RequestBody ProductRequestDto requestDto) {
+        return productMapper.toDto(productService.save(productMapper.toModel(requestDto)));
+    }
+}
