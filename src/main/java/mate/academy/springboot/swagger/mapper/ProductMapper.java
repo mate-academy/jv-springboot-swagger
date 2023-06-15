26e31d43aecbf0864ded86cfc089835c6988cc6b
Serhii Shyian <serhii.shn@gmail.com>
2023-06-15 10:49:44 +0300

    1) Added dependencies in pom.xml 2) Added properties to application.properties for H2 DB 3) Created Product class in model package 4) Created ProductRepository class in repository package 5) Created ProductService interface and it implementation in service package 6) Created Product request and response dto in dto package 7) Created DtoMapper and ProductMapper classes in mapper package 8) Created SortUtil class in util package 9) Created ProductController and InitialDataController in controller package

---
+++ src/main/java/mate/academy/springboot/swagger/mapper/ProductMapper.java
@@ -0,0 +1,28 @@
+package mate.academy.springboot.swagger.mapper;
+
+import mate.academy.springboot.swagger.dto.ProductRequestDto;
+import mate.academy.springboot.swagger.dto.ProductResponseDto;
+import mate.academy.springboot.swagger.model.Product;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ProductMapper implements
+        DtoMapper<Product, ProductRequestDto, ProductResponseDto> {
+
+    @Override
+    public Product mapToModel(ProductRequestDto dto) {
+        Product product = new Product();
+        product.setTitle(dto.getTitle());
+        product.setPrice(dto.getPrice());
+        return product;
+    }
+
+    @Override
+    public ProductResponseDto mapToDto(Product product) {
+        ProductResponseDto responseDto = new ProductResponseDto();
+        responseDto.setId(product.getId());
+        responseDto.setTitle(product.getTitle());
+        responseDto.setPrice(product.getPrice());
+        return responseDto;
+    }
+}
---
+++ src/main/java/mate/academy/springboot/swagger/service/impl/ProductServiceImpl.java
@@ -0,0 +1,61 @@
+package mate.academy.springboot.swagger.service.impl;
+
+import java.math.BigDecimal;
+import java.util.List;
+import mate.academy.springboot.swagger.model.Product;
+import mate.academy.springboot.swagger.repository.ProductRepository;
+import mate.academy.springboot.swagger.service.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ProductServiceImpl implements ProductService {
+    private final ProductRepository productRepository;
+
+    @Autowired
+    public ProductServiceImpl(ProductRepository productRepository) {
+        this.productRepository = productRepository;
+    }
+
+    @Override
+    public Product create(Product product) {
+        return productRepository.save(product);
+    }
+
+    @Override
+    public Product findById(Long id) {
+        return productRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Can't find a product by Id: " + id));
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        productRepository.deleteById(id);
+    }
+
+    @Override
+    public Product update(Product product) {
+        if (productRepository.existsById(product.getId())) {
+            return productRepository.saveAndFlush(product);
+        }
+        throw new RuntimeException("Can't update product: " + product + " not found");
+    }
+
+    @Override
+    public List<Product> findAll() {
+        return productRepository.findAll();
+    }
+
+    @Override
+    public List<Product> findAll(PageRequest pageRequest) {
+        return productRepository.findAll(pageRequest).toList();
+    }
+
+    @Override
+    public List<Product> findAllByPriceBetween(BigDecimal from,
+                                               BigDecimal to,
+                                               PageRequest pageRequest) {
+        return productRepository.findAllByPriceBetween(from, to, pageRequest);
+    }
+}
