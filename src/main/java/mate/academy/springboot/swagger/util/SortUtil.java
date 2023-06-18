7a7a1f4e9cbef63c3bd5dbabd8ee4cf4b9ca3953
Serhii Shyian <serhii.shn@gmail.com>
2023-06-18 20:24:56 +0300

    1) In SortUtil added constants SORT_DELIMITER, PARAM_DELIMITER

--- src/main/java/mate/academy/springboot/swagger/util/SortUtil.java
+++ src/main/java/mate/academy/springboot/swagger/util/SortUtil.java
@@ -7,14 +7,17 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class SortUtil {
+    private static final String SORT_DELIMITER = ":";
+    private static final String PARAM_DELIMITER = ";";
+
     public Sort sortData(String sortBy) {
         List<Sort.Order> orders = new ArrayList<>();
-        if (sortBy.contains(":")) {
-            String[] sortingFields = sortBy.split(";");
+        if (sortBy.contains(SORT_DELIMITER)) {
+            String[] sortingFields = sortBy.split(PARAM_DELIMITER);
             for (String field: sortingFields) {
                 Sort.Order order;
-                if (field.contains(":")) {
-                    String[] fieldsAndDirections = field.split(":");
+                if (field.contains(SORT_DELIMITER)) {
+                    String[] fieldsAndDirections = field.split(SORT_DELIMITER);
                     order = new Sort.Order(
                             Sort.Direction.valueOf(fieldsAndDirections[1]),
                             fieldsAndDirections[0]);
