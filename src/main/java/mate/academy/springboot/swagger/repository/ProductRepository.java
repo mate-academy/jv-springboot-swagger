3ca03b588b1de34426852f4666c27b6cd0a9acc4
Oleksandr Koval <0993079960a@gmail.com>
2023-07-19 11:02:05 +0300

    improved solution

--- src/main/java/mate/academy/springboot/swagger/repository/ProductRepository.java
+++ src/main/java/mate/academy/springboot/swagger/repository/ProductRepository.java
@@ -5,9 +5,7 @@ import java.util.List;
 import mate.academy.springboot.swagger.model.Product;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
-@Repository
 public interface ProductRepository extends JpaRepository<Product, Long> {
     List<Product> findAllByPriceBetween(BigDecimal from, BigDecimal to, Pageable pageable);
 }
--- src/main/java/mate/academy/springboot/swagger/service/mapper/ProductMapper.java
+++ src/main/java/mate/academy/springboot/swagger/service/mapper/ProductMapper.java
@@ -3,9 +3,9 @@ package mate.academy.springboot.swagger.service.mapper;
 import mate.academy.springboot.swagger.dto.request.ProductRequestDto;
 import mate.academy.springboot.swagger.dto.response.ProductResponseDto;
 import mate.academy.springboot.swagger.model.Product;
-import org.springframework.stereotype.Controller;
+import org.springframework.stereotype.Component;
 
-@Controller
+@Component
 public class ProductMapper implements DtoMapper<ProductRequestDto, ProductResponseDto, Product> {
     @Override
     public Product mapToModel(ProductRequestDto dto) {
