a2e60f3fb7b9d125c205b79a953f26e200ee9e16
V-Solianyk <112614843+V-Solianyk@users.noreply.github.com>
2023-08-21 19:33:02 +0300

    fixed one mistake.

--- src/main/java/mate/academy/springboot/swagger/service/impl/ProductServiceImpl.java
+++ src/main/java/mate/academy/springboot/swagger/service/impl/ProductServiceImpl.java
@@ -33,9 +33,7 @@ public class ProductServiceImpl implements ProductService {
 
     @Override
     public Product update(Product product, Long id) {
-        Product oldProduct = productRepository.findById(id)
-                .orElseThrow(() -> new NoSuchElementException("Product not found with this "
-                        + "id: " + id));
+        Product oldProduct = getById(id);
         oldProduct.setTitle(product.getTitle());
         oldProduct.setPrice(product.getPrice());
         return productRepository.save(oldProduct);
